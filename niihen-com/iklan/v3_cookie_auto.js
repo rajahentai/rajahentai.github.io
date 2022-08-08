bb9c02de2b29abae343f8c4d77135f5a90dd8528
NiiHen <63357664+NiiHen@users.noreply.github.com>
2022-08-08 18:47:16 +0700

    Update and rename v2_cookie_auto.js to v3_cookie_auto.js

--- niihen-com/iklan/v3_cookie_auto.js
+++ niihen-com/iklan/v3_cookie_auto.js
@@ -61,14 +61,9 @@ console.log(hitunganviewnya);
 
 var num = hitunganviewnya;
    //document.write("Number = "+num+"<br>");
-   if(num % 5 == 0) {
+   if(num % 3 == 0) {
       //document.write('Number is even!');
       //window.location='https://toddlecausebeeper.com/wd4gfq2p2f?key=a86b4c67e65aa35f34cbabf2a06c6951';
-      
-      var scriptxGHEA = document.createElement('script');
-      scriptxGHEA.setAttribute('type', 'text/javascript');
-      scriptxGHEA.setAttribute('src', 'https://toddlecausebeeper.com/7c/bf/44/7cbf4466a8b07263c55a0b678b300b43.js');
-      document.head.appendChild(scriptxGHEA); 
       
       var scriptxGHEAs = document.createElement('script');
       scriptxGHEAs.setAttribute('type', 'text/javascript');
@@ -76,12 +71,7 @@ var num = hitunganviewnya;
       document.head.appendChild(scriptxGHEAs);       
       
    } else {
-      //document.write('Number is odd!');
-      
-      var scriptxGHEAs = document.createElement('script');
-      scriptxGHEAs.setAttribute('type', 'text/javascript');
-      scriptxGHEAs.setAttribute('src', 'https://toddlecausebeeper.com/9c/da/18/9cda18f433411bb5174d0db2ee1bbffd.js');
-      document.head.appendChild(scriptxGHEAs);       
+      //document.write('Number is odd!');    
       
 }
 
